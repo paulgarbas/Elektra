0a5901ed2e05519331e2997d0bf12f779e3e6875
Paulius <garbasauskas@gmail.com>
2018-09-20 17:23:48 +0300

    home-slider, our-projects, our-brands-slider

---
+++ src/js/slider.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+    // Home slider
+    $('.home-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // responsive: [
+        //     {
+        //         breakpoint: 1200,
+        //         settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 3,
+        //         infinite: true,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 2
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 500,
+        //         settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+
+    // Our Brands Slider
+    $('.our-brands-slider').slick({
+        // autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        // responsive: [
+        //     {
+        //         breakpoint: 1200,
+        //         settings: {
+        //         slidesToShow: 4,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 1024,
+        //         settings: {
+        //         slidesToShow: 3,
+        //         slidesToScroll: 1,
+        //         infinite: true,
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 768,
+        //         settings: {
+        //         slidesToShow: 2,
+        //         slidesToScroll: 1
+        //         }
+        //     },
+        //     {
+        //         breakpoint: 500,
+        //         settings: {
+        //         slidesToShow: 1,
+        //         slidesToScroll: 1
+        //         }
+        //     }
+        // ]
+    });
+});
+
+
+
+
+
